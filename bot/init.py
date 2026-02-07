6a8396e4bfa3f3b3b721c49b4d4908eb1160f2f1
matheusdragons <mguimaraes.email@gmail.com>
2026-02-07 15:41:23 -0300

    Update init.py

--- bot/init.py
+++ bot/init.py
@@ -1,1 +1,11 @@
-# Pacote bot
+"""
+DragonBot SaaS - Módulo de Trading
+Versão: 2.0
+"""
+
+from .deriv_api import DerivAPI
+from .strategy import Strategy
+from .robot import TradingRobot
+
+__all__ = ['DerivAPI', 'Strategy', 'TradingRobot']
+__version__ = '2.0.0'
